f1d801c4064937309f0cfd2a38d18836dba07dda
Karthik-Saravanan-10 <karthiksaravanan909@gmail.com>
2024-12-20 17:46:07 +0530

    modifies passwordChecker

--- password_checker/index.js
+++ password_checker/index.js
@@ -4,21 +4,16 @@ const meter = document.querySelector(".meter");
 
 form.addEventListener("input", (e) => {
   e.preventDefault();
-  let index = 1;
+  let index = 0;
   const regEx = [/[0-9]/, /[A-Z]/, /[a-z]/, /[^0-9A-z]/];
-  const backColor = ["#A6C7A9", "red", "orange", "#a3ff00", "yellow", "green"];
-  const percentage = ["0px", "90px", "180px", "240px", "300px", "381px"];
-  regEx.forEach((element) => {
-    if (inputData.value.length >= 7) {
-      console.log("index1", index);
-      if (element.test(inputData.value)) {
-        index += 1;
-      }
-    } else if (inputData.value.length == 0) {
-      console.log("index2", index);
-      index = 0;
-    }
-  });
+  const backColor = ["red", "orange", "#a3ff00", "yellow", "green"];
+  const percentage = ["90px", "180px", "240px", "300px", "381px"];
+
+  if (inputData.value.length >= 7) {
+    regEx.forEach((element) => {
+      if (element.test(inputData.value)) index++;
+    });
+  }
 
   meter.style.backgroundColor = backColor[index];
   meter.style.width = percentage[index];
